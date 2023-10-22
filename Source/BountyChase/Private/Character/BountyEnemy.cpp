59f59f38a41a9b705394ebc7bf37ca3e5be608d7
Wrast <donec2509@gmail.com>
2023-10-22 03:39:06 +0300

    Added Highlight FX, Dash Input

--- Source/BountyChase/Private/Character/BountyEnemy.cpp
+++ Source/BountyChase/Private/Character/BountyEnemy.cpp
@@ -3,3 +3,20 @@
 
 #include "Character/BountyEnemy.h"
 
+ABountyEnemy::ABountyEnemy()
+{
+	GetMesh()->SetCustomDepthStencilValue(250);
+	Weapon->SetCustomDepthStencilValue(250);
+}
+
+void ABountyEnemy::HighlightActor()
+{
+	GetMesh()->SetRenderCustomDepth(true);
+	Weapon->SetRenderCustomDepth(true);
+}
+
+void ABountyEnemy::UnHighlightActor()
+{
+	GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
+}
--- Source/BountyChase/Public/Character/BountyEnemy.h
+++ Source/BountyChase/Public/Character/BountyEnemy.h
@@ -4,14 +4,18 @@
 
 #include "CoreMinimal.h"
 #include "Character/BountyCharacterBase.h"
+#include "Interaction/HighlightInterface.h"
 #include "BountyEnemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class BOUNTYCHASE_API ABountyEnemy : public ABountyCharacterBase
+class BOUNTYCHASE_API ABountyEnemy : public ABountyCharacterBase, public IHighlightInterface
 {
 	GENERATED_BODY()
-	
+public:
+	ABountyEnemy();
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
 };
